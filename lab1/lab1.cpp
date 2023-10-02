078a9266cd9487d665fb11355270b88ce78d2664
Pavlo Hladkyy <132133261+pavlohladkyy@users.noreply.github.com>
2023-10-02 11:21:34 +0300

    added changes to functions A and B

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,39 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
-class Calculator {
+class calc {
 public:
     // Оголошення публічних змінних класу
     float x, y, z;
 
     // Конструктор класу, приймає вхідні параметри і ініціалізує змінні класу
-    Calculator(float x, float y, float z) {
+    calc(float x, float y, float z) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
 
     // Метод для обчислення змінної a
-    double calculateA() {
-       
-        double a = pow(y, 2) + ((z / pow(x, 2)) + (pow(sin(x), 2))) / (fabs((pow(x, 2) + y) / ((z + pow(x, 3) / 3)) - fabs(log(pow(x, 2) + z))));
+    double A() {
+        double b = B();  // Викликаємо метод calculateB для обчислення b
+        double a = pow(y, 2) + ((z / pow(x, 2)) + (pow(sin(b), 2))) / (fabs((pow(x, 2) + y) / ((z + pow(x, 3) / 3)) - fabs(log(pow(x, 2) + z))));
         return a;
     }
-
     // Метод для обчислення змінної b
-    double calculateB() {
-       
-        
-        double b = (pow(fabs(x + y / fabs(pow(z, 0.6)) + (x + pow(z, 2) / (2 * x + y))), 1 / 3) - (z * exp((pow(x, 2) - y) / (1 + x))));
+    double B() {
+        double b = (pow(fabs((x + y) / fabs(pow(z, 0.6)) + sin((x + pow(z, 2)) / (2 * x + y))), 1.0 / 3.0) - (z * exp((pow(x, 2) - y) / (1 + x))));
         return b;
     }
 
-private:
+
     // Приватний метод, який викликає метод calculateB
-    double b() {
-        return calculateB();
-    }
 };
 
 int main() {
@@ -41,14 +35,13 @@ int main() {
     float x = 0.48 * 14;
     float y = 0.47 * 14;
     float z = -1.32 * 14;
-    int factorial = 2;
 
     // Створення об'єкта класу Calculator 
-    Calculator calc(x, y, z);
+    calc calc(x, y, z);
 
     // Обчислення 'a' і 'b' за допомогою методів класу Calculator
-    double a = calc.calculateA();
-    double b = calc.calculateB();
+    double a = calc.A();
+    double b = calc.B();
 
     cout << "a: " << a << endl;
     cout << "b: " << b << endl;
